c45665c6bab7aed69217f48c8ce52284d8e2a84c
mrkriodev <kriolord@gmail.com>
2026-06-02 14:38:45 +0000

    add workflow and update.sh

---
+++ update.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e  # Останавливаем скрипт при ошибке
+set -o pipefail  # Учитываем статус выхода из команд в конвейере
+
+# Функция для обработки ошибок
+error_handler() {
+    echo "Произошла ошибка в строке $1."
+    exit 1
+}
+
+# Обработчик ошибок
+trap 'error_handler $LINENO' ERR
+
+
+#echo "Обновляем репозиторий..."
+git pull
+git fetch --all
+git reset --hard origin/main
+
+echo "Останавливаем и удаляем старые контейнеры и образы..."
+docker compose down --rmi all
+
+echo "Очищаем директорию сайта..."
+rm -rf html/*
+
+echo "Запускаем контейнеры в фоне..."
+docker compose up -d
+
+echo "Перемещаем содержимое nginx html..."
+docker compose exec frontend sh -c "mv /usr/share/nginx/html/* /tmp/"
+
+echo "Останавливаем контейнер..."
+docker compose down --rmi all
+
+echo "Синхронизируем файлы с nginx..."
+rsync -r --delete html/* ../../nginx/html/
+
+echo "Перезапускаем nginx..."
+docker exec nginx bash -c "nginx -s reload"
+
+echo "Скрипт выполнен успешно!"
